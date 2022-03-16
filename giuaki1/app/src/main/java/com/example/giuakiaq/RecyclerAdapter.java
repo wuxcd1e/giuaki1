1824a47edb9b453705b66fdeda9478b1912a507a
wuxcd1e <100299732+wuxcd1e@users.noreply.github.com>
2022-03-16 23:33:45 +0700

    Add files via upload

---
+++ giuaki1/app/src/main/java/com/example/giuakiaq/RecyclerAdapter.java
@@ -0,0 +1,21 @@
+package com.example.demo
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class RecyclerAdapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerAdapter.ViewHolder {
+        TODO("Not yet implemented")
+        }
+
+        override fun onBindViewHolder(holder: RecyclerAdapter.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+        }
+
+        override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+        }
+        init class view  {
+
+}
+}
